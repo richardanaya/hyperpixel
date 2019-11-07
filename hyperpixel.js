9e9786c2c18eaf08f39ff7d8da2c974f6b72afb5
Richard Anaya <richard.anaya@gmail.com>
2019-11-06 22:33:37 -0800

    Update hyperpixel.js

--- hyperpixel.js
+++ hyperpixel.js
@@ -7,10 +7,8 @@ var HyperPixel = function(canvas){
 
     this.scene = new THREE.Scene();
     this.scene.fog = new THREE.Fog( 0x050505, 2000, 3500 );
-
     this.renderer = null;
     this.particleSystem = null;
-
     this.init();
     this.animate();
 };
@@ -32,31 +30,15 @@ HyperPixel.prototype.setColor = function(x,y,r,g,b){
 };
 
 HyperPixel.prototype.update = function() {
-    this.particleSystem.geometry.attributes.color.array = this.colors;
-    this.particleSystem.geometry.colorsNeedUpdate = true;
-};
-
-HyperPixel.prototype.getElement = function(){
-    return this.renderer.domElement;
+    this.particleSystem.geometry.attributes.color.needsUpdate = true;
 };
 
 HyperPixel.prototype.init = function() {
     var particles = this.width*this.height;
 
     var geometry = new THREE.BufferGeometry();
-    geometry.attributes = {
-
-        position: {
-            itemSize: 3,
-            array: new Float32Array( particles * 3 ),
-            numItems: particles * 3
-        },
-        color: {
-            itemSize: 3,
-            array: new Float32Array( particles * 3 ),
-            numItems: particles * 3
-        }
-    }
+    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( new Array(particles*3), 3 ) );
+	geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( new Array(particles*3), 3 ) );
 
     var positions = geometry.attributes.position.array;
     this.colors = geometry.attributes.color.array;
@@ -75,9 +57,8 @@ HyperPixel.prototype.init = function() {
         }
     }
 
-    var material = new THREE.ParticleBasicMaterial( { size: 1, vertexColors: true } );
-
-    this.particleSystem = new THREE.ParticleSystem( geometry, material );
+    var material = new THREE.PointsMaterial( { size: 15, vertexColors: THREE.VertexColors } );
+    this.particleSystem = points = new THREE.Points( geometry, material );
     this.scene.add( this.particleSystem );
 
     this.renderer = new THREE.WebGLRenderer( {canvas: this.canvas, antialias: false, clearColor: 0x333333, clearAlpha: 1, alpha: false } );
@@ -87,7 +68,5 @@ HyperPixel.prototype.init = function() {
 
 HyperPixel.prototype.animate = function() {
     requestAnimationFrame( this.animate.bind(this) );
-    var w = this.width;
-    var h = this.height;
     this.renderer.render( this.scene, this.camera );
 };
